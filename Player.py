c0d75dd9c4b28a6bc40b4fca97145df9b82a94dc
Yusuf Hassan <yusufhassan@Yusufs-MacBook-Air-2.local>
2020-07-16 16:54:42 -0500

    my first commit

---
+++ Player.py
@@ -0,0 +1,13 @@
+class Player:
+  def __init__(self,first_name, last_name, age, PPG, ASG, Plus_Minus):
+    self.first_name = name
+    self.last_name = name
+    self.age = age
+    self.PPG = PPG
+    self.ASG = ASG
+    self.Plus_Minus = Plus_Minus
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Player("John", 36)
+p1.myfunc()
